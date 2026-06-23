c2cc8508b1ada000b9ef9c7ad88e43be2be94a9b
USUARIO <USUARIO@DESKTOP-IHK1JIR>
2026-06-22 22:04:24 -0400

    Agregar archivos de proyecto.

---
+++ LOGIN/Views/Shared/_LayoutTienda.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LOGIN.Views.Shared
+{
+    public class _LayoutTiendaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
